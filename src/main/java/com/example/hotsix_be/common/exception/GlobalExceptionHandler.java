7962f4abf4bce8e33f220b5fec55ec73d838df90
kyumho kim <js030@github.com>
2024-01-22 13:13:31 +0900

    feat : Exception 핸들러 추가

--- src/main/java/com/example/hotsix_be/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/hotsix_be/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.hotsix_be.common.exception;
 
+import static com.example.hotsix_be.common.exception.ExceptionCode.INTERNAL_SEVER_ERROR;
 import static com.example.hotsix_be.common.exception.ExceptionCode.INVALID_REQUEST;
 
 import java.util.Objects;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -28,6 +30,22 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         final String errMessage = Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage();
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(INVALID_REQUEST.getCode(), errMessage));
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ExceptionResponse> handleBadRequestException(final BadRequestException e) {
+        log.warn(e.getMessage(), e);
+
+        return ResponseEntity.badRequest()
+                .body(new ExceptionResponse(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(final Exception e) {
+        log.error(e.getMessage(), e);
+
+        return ResponseEntity.internalServerError()
+                .body(new ExceptionResponse(INTERNAL_SEVER_ERROR.getCode(), INTERNAL_SEVER_ERROR.getMessage()));
     }
 
 }
