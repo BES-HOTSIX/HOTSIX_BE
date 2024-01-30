fe8458f689cfaac951c067ea3844165edb5e9b89
hagd <146146134+hagd0520@users.noreply.github.com>
2024-01-30 17:48:10 +0900

    핫쉐어 김경환 (결제) (#8)
    * feat: Add addCash, payByCashOnly on CashLogService and etc
    
    * feat: Add tossPayment
    
    * feat: git pull
    
    * feat: git pull
    
    * feat: git pull
    
    * feat: git pull
    
    * feat: CashLogConfirm

--- src/main/java/com/example/hotsix_be/member/entity/Member.java
+++ src/main/java/com/example/hotsix_be/member/entity/Member.java
@@ -1,30 +1,11 @@
 package com.example.hotsix_be.member.entity;
 
-import static jakarta.persistence.EnumType.STRING;
-import static jakarta.persistence.GenerationType.IDENTITY;
-
 import com.example.hotsix_be.common.entity.DateEntity;
-import com.example.hotsix_be.login.domain.RefreshToken;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import java.time.LocalDateTime;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "members")
@@ -46,7 +27,7 @@ public class Member extends DateEntity {
     @Column(unique = true)
     private String nickname;
 
-    private Long restCash;
+    private long restCash;
 
     @Enumerated(EnumType.STRING)
     private SocialProvider socialProvider;
@@ -82,4 +63,7 @@ public class Member extends DateEntity {
         return !nickname.equals(inputNickname);
     }
 
+    public void updateRestCash(Long newRestCash) {
+        this.restCash = newRestCash;
+    }
 }
