13905f1cfd5757766f29e562083f7a283f8c1a60
jkeum-dev <jon9eun@gmail.com>
2024-01-30 16:54:01 +0900

    feat : 예약하기 POST
    예약하기 버튼을 누르면 예약 정보 POST, 응답으로 reserveId를 줌
    Reservation entity에 isPaid 추가

---
+++ src/main/java/com/example/hotsix_be/reservation/dto/response/ReservationCreateResponse.java
@@ -0,0 +1,19 @@
+package com.example.hotsix_be.reservation.dto.response;
+
+import com.example.hotsix_be.reservation.entity.Reservation;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@RequiredArgsConstructor(access = PRIVATE)
+public class ReservationCreateResponse {
+	private final Long id;
+
+	public static ReservationCreateResponse of(final Reservation reservation) {
+		return new ReservationCreateResponse(
+				reservation.getId()
+		);
+	}
+}
--- src/main/java/com/example/hotsix_be/reservation/service/ReservationService.java
+++ src/main/java/com/example/hotsix_be/reservation/service/ReservationService.java
@@ -1,5 +1,9 @@
 package com.example.hotsix_be.reservation.service;
 
+import com.example.hotsix_be.hotel.entity.Hotel;
+import com.example.hotsix_be.hotel.exception.HotelException;
+import com.example.hotsix_be.hotel.repository.HotelRepository;
+import com.example.hotsix_be.reservation.dto.request.ReservationInfoRequest;
 import com.example.hotsix_be.reservation.dto.response.ReservationDetailResponse;
 import com.example.hotsix_be.reservation.entity.Reservation;
 import com.example.hotsix_be.reservation.exception.ReservationException;
@@ -7,12 +11,14 @@ import com.example.hotsix_be.reservation.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import static com.example.hotsix_be.common.exception.ExceptionCode.NOT_FOUND_HOTEL_ID;
 import static com.example.hotsix_be.common.exception.ExceptionCode.NOT_FOUND_RESERVATION_ID;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
 	private final ReservationRepository reservationRepository;
+	private final HotelRepository hotelRepository;
 
 	public ReservationDetailResponse findById(Long id) {
 		Reservation reservation = reservationRepository.findById(id).orElseThrow(() -> new ReservationException(NOT_FOUND_RESERVATION_ID));
@@ -21,5 +27,20 @@ public class ReservationService {
 				reservation.getHotel(),
 				reservation
 		);
+	}
+
+	public Reservation save(final Long hotelId, final ReservationInfoRequest reservationInfoRequest) {
+		Hotel hotel = hotelRepository.findById(hotelId).orElseThrow(() -> new HotelException(NOT_FOUND_HOTEL_ID));
+
+		final Reservation reservation = new Reservation(
+				reservationInfoRequest.getCheckInDate(),
+				reservationInfoRequest.getCheckOutDate(),
+				reservationInfoRequest.getNumOfGuests(),
+				reservationInfoRequest.getPrice(),
+				hotel,
+				reservationInfoRequest.isPaid()
+		);
+
+		return reservationRepository.save(reservation);
 	}
 }
