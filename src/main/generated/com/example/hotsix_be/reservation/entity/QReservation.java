b1d3551e00ac98cd00e5177f13c04bfc32565efd
hagd <hagd0520@gmail.com>
2024-02-02 17:46:13 +0900

    feat: git pull

---
+++ src/main/generated/com/example/hotsix_be/reservation/entity/QReservation.java
@@ -0,0 +1,77 @@
+package com.example.hotsix_be.reservation.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QReservation is a Querydsl query type for Reservation
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReservation extends EntityPathBase<Reservation> {
+
+    private static final long serialVersionUID = -690041067L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QReservation reservation = new QReservation("reservation");
+
+    public final com.example.hotsix_be.common.entity.QDateEntity _super = new com.example.hotsix_be.common.entity.QDateEntity(this);
+
+    public final DateTimePath<java.time.LocalDateTime> cancelDate = createDateTime("cancelDate", java.time.LocalDateTime.class);
+
+    public final DateTimePath<java.time.LocalDateTime> checkInDate = createDateTime("checkInDate", java.time.LocalDateTime.class);
+
+    public final DateTimePath<java.time.LocalDateTime> checkOutDate = createDateTime("checkOutDate", java.time.LocalDateTime.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> guests = createNumber("guests", Long.class);
+
+    public final com.example.hotsix_be.hotel.entity.QHotel hotel;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isPaid = createBoolean("isPaid");
+
+    public final com.example.hotsix_be.member.entity.QMember member;
+
+    public final NumberPath<Long> price = createNumber("price", Long.class);
+
+    public final com.example.hotsix_be.review.entity.QReview review;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public QReservation(String variable) {
+        this(Reservation.class, forVariable(variable), INITS);
+    }
+
+    public QReservation(Path<? extends Reservation> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QReservation(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QReservation(PathMetadata metadata, PathInits inits) {
+        this(Reservation.class, metadata, inits);
+    }
+
+    public QReservation(Class<? extends Reservation> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.hotel = inits.isInitialized("hotel") ? new com.example.hotsix_be.hotel.entity.QHotel(forProperty("hotel"), inits.get("hotel")) : null;
+        this.member = inits.isInitialized("member") ? new com.example.hotsix_be.member.entity.QMember(forProperty("member")) : null;
+        this.review = inits.isInitialized("review") ? new com.example.hotsix_be.review.entity.QReview(forProperty("review"), inits.get("review")) : null;
+    }
+
+}
+
