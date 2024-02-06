2c0b64891371cb1900689f5b5b7895289e075cc9
kyumho kim <js030@github.com>
2024-02-06 21:31:02 +0900

    feat : DataInit 사진 수정

--- src/main/java/com/example/hotsix_be/common/DataInit.java
+++ src/main/java/com/example/hotsix_be/common/DataInit.java
@@ -49,13 +49,17 @@ public class DataInit implements ApplicationRunner {
                 Hotel hotel = new Hotel("호텔", "서울" + i, "구로구" + i, 3L, 2L, 3L, 5L, List.of("주방"), "test" + i,
                         "test" + i, 10000L, member);
 
-                hotel.addImage(new Image("test" + i, "test" + i, "https://kr.object.ncloudstorage.com/hotsix.accomodation.bucket/ACCOMODATION/2024/01/ACCOMODATION_123123_0926ac58-3dd4-4b2c-a044-e123f2d20e6b_%ED%8E%9C%EC%85%982.jpg", 30L));
+                hotel.addImage(new Image("test" + i, "test" + i,
+                        "https://kr.object.ncloudstorage.com/hotsix.accomodation.bucket/ACCOMMODATION/2024/02/ACCOMMODATION_123_1afd0e02-780f-4732-8d62-a7453667d9de_pension1.jpg",
+                        30L));
                 hotelRepository.save(hotel);
             });
 
             IntStream.rangeClosed(1, 20).forEach(j -> {
-                String sql = "INSERT INTO reservations (check_in_date, check_out_date, guests, price, hotel_id, member_id, is_paid) " +
-                        "VALUES (?, ?, ?, ?, (SELECT id FROM hotels WHERE id = ?), (SELECT id FROM members ORDER BY id DESC LIMIT 1), ?)";
+                String sql =
+                        "INSERT INTO reservations (check_in_date, check_out_date, guests, price, hotel_id, member_id, is_paid) "
+                                +
+                                "VALUES (?, ?, ?, ?, (SELECT id FROM hotels WHERE id = ?), (SELECT id FROM members ORDER BY id DESC LIMIT 1), ?)";
 
                 jdbcTemplate.update(sql, startDate, endDate, 3 + j, 550000L, 1L, true);
             });
