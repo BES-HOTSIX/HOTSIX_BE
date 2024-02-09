b054ca17f692bd79e437306dbb2b3f9516dd6b84
js030 <js030@github.com>
2024-02-10 04:39:57 +0900

    feat: async 처리에도 로그에 추가 정보 담아줄 수 있도록

---
+++ src/main/java/com/example/hotsix_be/common/exception/AsyncExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.hotsix_be.common.exception;
+
+import java.lang.reflect.Method;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+
+    @Override
+    public void handleUncaughtException(final Throwable ex, final Method method, final Object... params) {
+        log.error(ex.getMessage(), ex);
+    }
+}
