f669d07ba0d627d47a41f2b538ac1e142f00ebce
hagd <hagd0520@gmail.com>
2024-02-14 14:22:23 +0900

    fix

--- src/test/java/com/example/hotsix_be/login/service/LoginServiceTest.java
+++ src/test/java/com/example/hotsix_be/login/service/LoginServiceTest.java
@@ -1,104 +1,5 @@
-<<<<<<< HEAD
-//package com.example.hotsix_be.login.service;
-//
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.when;
-//
-//import com.example.hotsix_be.common.exception.AuthException;
-//import com.example.hotsix_be.login.domain.MemberTokens;
-//import com.example.hotsix_be.login.domain.RefreshToken;
-//import com.example.hotsix_be.login.dto.request.LoginRequest;
-//import com.example.hotsix_be.login.dto.response.LoginResponse;
-//import com.example.hotsix_be.login.repository.RefreshTokenRepository;
-//import com.example.hotsix_be.login.util.JwtProvider;
-//import com.example.hotsix_be.member.entity.Member;
-//import com.example.hotsix_be.member.repository.MemberRepository;
-//import java.util.Optional;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@ExtendWith(MockitoExtension.class)
-//@Transactional
-//class LoginServiceTest {
-//
-//    @Mock
-//    private MemberRepository memberRepository;
-//
-//    @Mock
-//    private PasswordEncoder passwordEncoder;
-//
-//    @Mock
-//    private JwtProvider jwtProvider;
-//
-//    @Mock
-//    private RefreshTokenRepository refreshTokenRepository;
-//
-//    @InjectMocks
-//    private LoginService loginService;
-//
-//
-//    @Test
-//    void 로그인_성공() {
-//        // given
-//        LoginRequest loginRequest = new LoginRequest("username", "password");
-//        // Member 객체 생성 시 passwordEncoder.encode("password")로 비밀번호 인코딩
-//        Member member = new Member("username", passwordEncoder.encode("password"), "user1");
-//
-//        member = member.toBuilder()
-//                .id(1L)
-//                .build();
-//
-//        MemberTokens memberTokens = new MemberTokens("dummyRefreshToken", "dummyAccessToken");
-//        given(jwtProvider.generateLoginToken(member.getId().toString())).willReturn(memberTokens);
-//        given(passwordEncoder.matches(loginRequest.getPassword(), member.getPassword())).willReturn(true);
-//
-//        RefreshToken savedRefreshToken = new RefreshToken(memberTokens.getRefreshToken(), member.getId());
-//        given(refreshTokenRepository.save(any(RefreshToken.class))).willReturn(savedRefreshToken);
-//
-//        // when
-//        LoginResponse result = loginService.login(loginRequest, member);
-//
-//        // then
-//        assertThat(result.getAccessToken()).isEqualTo(memberTokens.getAccessToken());
-//        assertThat(result.getRefreshToken()).isEqualTo(memberTokens.getRefreshToken());
-//    }
-//
-//    @Test
-//    void 로그인_실패_비밀번호_불일치() {
-//        // given
-//        String username = "username";
-//        String wrongPassword = "wrongPassword";
-//        Member member = new Member(username, passwordEncoder.encode("password"), "user1");
-//
-//        LoginRequest loginRequest = new LoginRequest(username, wrongPassword);
-//
-//        when(passwordEncoder.matches(wrongPassword, member.getPassword())).thenReturn(false);
-//
-//        // when & then
-//        assertThrows(AuthException.class, () -> loginService.login(loginRequest, member));
-//    }
-//
-//
-//}
-=======
-
 package com.example.hotsix_be.login.service;
 
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
 
 import com.example.hotsix_be.common.exception.AuthException;
 import com.example.hotsix_be.login.domain.MemberTokens;
@@ -116,6 +17,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -184,5 +91,3 @@ class LoginServiceTest {
     }
 
 }
-
->>>>>>> 2baeb0c1c7c28de853ccd48d7251a090ad8b28f8
