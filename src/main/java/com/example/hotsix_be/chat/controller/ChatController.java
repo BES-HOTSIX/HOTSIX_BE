9270e079d02cdc5f39a62818e1cca46db18aaf73
jkeum-dev <jon9eun@gmail.com>
2024-03-07 13:29:24 +0900

    refactor : 채팅 api와 웹소켓 컨트롤러 구분

--- src/main/java/com/example/hotsix_be/chat/controller/ChatController.java
+++ src/main/java/com/example/hotsix_be/chat/controller/ChatController.java
@@ -3,9 +3,7 @@ package com.example.hotsix_be.chat.controller;
 import com.example.hotsix_be.auth.Auth;
 import com.example.hotsix_be.auth.MemberOnly;
 import com.example.hotsix_be.auth.util.Accessor;
-import com.example.hotsix_be.chat.dto.request.ChatMessageRequest;
 import com.example.hotsix_be.chat.dto.request.ChatRoomCreateRequest;
-import com.example.hotsix_be.chat.dto.response.ChatMessageResponse;
 import com.example.hotsix_be.chat.dto.response.ChatRoomCreateResponse;
 import com.example.hotsix_be.chat.dto.response.ChatRoomInfoResponse;
 import com.example.hotsix_be.chat.service.ChatService;
@@ -13,18 +11,15 @@ import com.example.hotsix_be.common.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/chat")
 public class ChatController {
-	private final SimpMessagingTemplate messagingTemplate;
 	private final ChatService chatService;
 
-	@PostMapping("/api/v1/chat/create")
+	@PostMapping("/create")
 	@MemberOnly
 	public ResponseEntity<?> createChatRoom(
 			@RequestBody final ChatRoomCreateRequest chatRoomCreateRequest,
@@ -41,7 +36,7 @@ public class ChatController {
 		);
 	}
 
-	@GetMapping("/api/v1/chat/info/{roomId}")
+	@GetMapping("/info/{roomId}")
 	@MemberOnly
 	public ResponseEntity<?> getChatRoom(
 			@PathVariable(value = "roomId") final Long roomId,
@@ -56,11 +51,5 @@ public class ChatController {
 						null, chatRoomInfoResponse
 				)
 		);
-	}
-
-	@MessageMapping("/chat.sendMessage/{roomId}")
-	public void sendMessage(final @DestinationVariable Long roomId, final ChatMessageRequest chatMessageRequest) {
-		ChatMessageResponse chatMessageResponse = chatService.saveMessage(roomId, chatMessageRequest);
-		messagingTemplate.convertAndSend(String.format("/topic/messages/%s", roomId), chatMessageResponse);
 	}
 }
