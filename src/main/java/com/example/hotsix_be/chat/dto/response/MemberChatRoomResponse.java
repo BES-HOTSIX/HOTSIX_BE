5ad4eec0bb9e3bd841a107a397b5f61053f43e6c
jkeum-dev <jon9eun@gmail.com>
2024-03-08 13:12:56 +0900

    feat : 마이페이지 나의 문의내역을 위한 DTO

---
+++ src/main/java/com/example/hotsix_be/chat/dto/response/MemberChatRoomResponse.java
@@ -0,0 +1,28 @@
+package com.example.hotsix_be.chat.dto.response;
+
+import com.example.hotsix_be.chat.entity.ChatRoom;
+import com.example.hotsix_be.member.entity.Member;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@RequiredArgsConstructor(access = PRIVATE)
+public class MemberChatRoomResponse {
+	private final Long chatRoomId;
+	private final String contactNickname;
+	private final String contactImage;
+	private final LocalDateTime latestDate;
+
+	public MemberChatRoomResponse of(final ChatRoom chatRoom, final Member contact, final LocalDateTime latestDate) {
+		return new MemberChatRoomResponse(
+				chatRoom.getId(),
+				contact.getNickname(),
+				contact.getImageUrl(),
+				latestDate
+		);
+	}
+}
