e230b0cfbcad14b41cba687d73c0b04369082028
kyumho kim <js030@github.com>
2024-03-13 16:01:03 +0900

    feat : 쿠폰 관련 CRUD 패키지 생성

---
+++ src/main/java/com/example/hotsix_be/coupon/controller/CouponController.java
@@ -0,0 +1,15 @@
+package com.example.hotsix_be.coupon.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/coupons")
+public class CouponController {
+
+
+}
---
+++ src/main/java/com/example/hotsix_be/coupon/service/CouponService.java
@@ -0,0 +1,13 @@
+package com.example.hotsix_be.coupon.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CouponService {
+
+
+}
