97b5e4f7378c057cf03eda5f7d56bff6c021a937
jjy990412 <wjdwn282@gmail.com>
2024-03-13 17:15:51 +0900

    .

--- src/main/java/com/example/hotsix_be/locations/LocationInfo.java
+++ src/main/java/com/example/hotsix_be/locations/LocationInfo.java
@@ -1,7 +1,11 @@
 package com.example.hotsix_be.locations;
 
+import org.locationtech.jts.geom.Point;
+
 public interface LocationInfo {
     String getName();
 
     String getAddress();
+
+    Point getLocation();
 }
