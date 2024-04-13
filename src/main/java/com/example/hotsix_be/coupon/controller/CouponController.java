c483b1fa81a7ab2326730b3d9ba70577dcb1a0cf
kyumho kim <js030@github.com>
2024-04-13 17:03:27 +0900

    deploy : fly.io 재배포 테스트2

--- src/main/java/com/example/hotsix_be/coupon/controller/CouponController.java
+++ src/main/java/com/example/hotsix_be/coupon/controller/CouponController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// 재배포
 @Log4j2
 @RestController
 @RequiredArgsConstructor
